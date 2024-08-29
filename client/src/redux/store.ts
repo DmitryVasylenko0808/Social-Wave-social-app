2b9db67f49ff144748491b210e5f0c67f37de595
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-08-29 16:38:46 +0300

    create client

---
+++ client/src/redux/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore({
+    reducer: {
+    },
+    middleware: (getDefaultMiddeware) => getDefaultMiddeware()
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+type DispatchFunc = () => AppDispatch;
+export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
