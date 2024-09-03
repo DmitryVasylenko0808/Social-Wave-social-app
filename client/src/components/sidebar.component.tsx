de02e2b577dc1fff0e841cc0a286f84551ae1a14
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-09-03 16:02:40 +0300

    update sidebar

--- client/src/components/sidebar.component.tsx
+++ client/src/components/sidebar.component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { House, Bookmark, UserRound, LogOut } from "lucide-react";
+import { House, Bookmark, UserRound, LogOut, LogIn } from "lucide-react";
 import { Button } from "../common/ui";
 import { useAuth } from "../hooks/useAuth";
 
@@ -8,7 +8,7 @@ const SideBar = () => {
   const { isAuthenticated, user, logOut } = useAuth();
 
   return (
-    <aside className="min-h-screen pr-11 border-r border-secondary-50">
+    <aside className="min-w-[180px] min-h-screen pr-3 border-r border-secondary-50">
       <div className="sticky top-0 pt-5">
         <ul className="flex flex-col text-base text-secondary-100 font-medium">
           <li className="block">
@@ -38,10 +38,15 @@ const SideBar = () => {
           )}
         </ul>
         <div className="my-2 w-full border border-secondary-50" />
-        {isAuthenticated && (
+        {isAuthenticated ? (
           <Button variant="terciary" onClick={logOut}>
             <LogOut />
             Log Out
+          </Button>
+        ) : (
+          <Button as="link" variant="terciary" to="/auth/sign-in">
+            <LogIn />
+            Sign In
           </Button>
         )}
       </div>
