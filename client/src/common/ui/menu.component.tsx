eee3f8313f80005b38358a0f688501055560ddf1
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-09-07 21:17:05 +0300

    update menu component

--- client/src/common/ui/menu.component.tsx
+++ client/src/common/ui/menu.component.tsx
@@ -1,19 +1,25 @@
-import { PropsWithChildren } from "react";
+import { ComponentProps, forwardRef } from "react";
 
-type MenuProps = PropsWithChildren & {
+type MenuProps = ComponentProps<"div"> & {
   open: boolean;
 };
 
-const Menu = ({ open, children }: MenuProps) => {
-  if (!open) {
-    return null;
-  }
+const Menu = forwardRef<HTMLDivElement, MenuProps>(
+  ({ open, children, ...menuProps }, ref) => {
+    if (!open) {
+      return null;
+    }
 
-  return (
-    <div className="absolute top-11 right-0 z-10 px-5 py-4 bg-white shadow-xl border border-textFieldBorder rounded-xl">
-      <ul className="flex flex-col gap-2">{children}</ul>
-    </div>
-  );
-};
+    return (
+      <div
+        className="absolute top-11 right-0 z-10 px-5 py-4 bg-white shadow-xl border border-textFieldBorder rounded-xl"
+        ref={ref}
+        {...menuProps}
+      >
+        <ul className="flex flex-col gap-2">{children}</ul>
+      </div>
+    );
+  }
+);
 
 export default Menu;
