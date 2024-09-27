16a0cae4fd617e9ab361115204f404fa6b2c638b
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-09-27 16:36:12 +0300

    update styles skeletons

--- client/src/common/components/article.skeleton.component.tsx
+++ client/src/common/components/article.skeleton.component.tsx
@@ -3,14 +3,14 @@ const ArticleSkeleton = () => {
     <div className="animate-pulse pb-16">
       <div className="mb-4 flex justify-between items-center">
         <div className="flex items-center gap-4">
-          <div className="w-[60px] h-[60px] bg-gray-200 rounded-full" />
-          <div className="w-28 h-4 bg-gray-200 rounded-full" />
+          <div className="w-[60px] h-[60px] bg-gray-200 rounded-full dark:bg-dark-300" />
+          <div className="w-28 h-4 bg-gray-200 rounded-full dark:bg-dark-300" />
         </div>
-        <div className="w-10 h-5 bg-gray-200 rounded-full" />
+        <div className="w-10 h-5 bg-gray-200 rounded-full dark:bg-dark-300" />
       </div>
       <div>
-        <div className="w-full h-5 mb-3 bg-gray-200 rounded-full" />
-        <div className="w-80 h-5 mb-0.5 bg-gray-200 rounded-full" />
+        <div className="w-full h-5 mb-3 bg-gray-200 rounded-full dark:bg-dark-300" />
+        <div className="w-80 h-5 mb-0.5 bg-gray-200 rounded-full dark:bg-dark-300" />
       </div>
     </div>
   );
--- client/src/common/components/user.profile.skeleton.component.tsx
+++ client/src/common/components/user.profile.skeleton.component.tsx
@@ -1,16 +1,16 @@
 const UserProfileSkeleton = () => {
   return (
     <div className="animate-pulse">
-      <div className="h-cover bg-gray-200" />
+      <div className="h-cover bg-gray-200 dark:bg-dark-300" />
       <div className="px-6">
         <div className="relative bottom-12 flex items-center gap-3.5">
-          <div className="w-[90px] h-[90px] bg-gray-200 border-4 border-white rounded-full" />
-          <div className="mt-8 h-[20px] w-32 bg-gray-200 rounded-full" />
+          <div className="w-[90px] h-[90px] bg-gray-200 border-4 border-white rounded-full dark:bg-dark-300 dark:border-dark-100" />
+          <div className="mt-8 h-[20px] w-32 bg-gray-200 rounded-full dark:bg-dark-300" />
         </div>
-        <div className="mb-10 w-80 bg-gray-200 rounded-full" />
+        <div className="mb-10 w-80 bg-gray-200 rounded-full dark:bg-dark-300" />
         <div className="pb-7 flex gap-7">
-          <div className="h-[14px] w-20 bg-gray-200 rounded-full" />
-          <div className="h-[14px] w-20 bg-gray-200 rounded-full" />
+          <div className="h-[14px] w-20 bg-gray-200 rounded-full dark:bg-dark-300" />
+          <div className="h-[14px] w-20 bg-gray-200 rounded-full dark:bg-dark-300" />
         </div>
       </div>
     </div>
