9c74683fad03eab5fd16a362c8cc883183eae84f
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-10-17 14:26:17 +0300

    update list item component (add ref)

--- client/src/modules/common/ui/list.item.component.tsx
+++ client/src/modules/common/ui/list.item.component.tsx
@@ -1,12 +1,18 @@
-import { ComponentProps, memo } from "react";
+import { ComponentProps, forwardRef, memo } from "react";
 import { cn } from "../../../utils/cn";
 
 type ListItemProps = ComponentProps<"li">;
 
-const ListItem = ({ className, children }: ListItemProps) => {
-  const classes = cn("block", className);
+const ListItem = forwardRef<HTMLLIElement, ListItemProps>(
+  ({ className, children }, ref) => {
+    const classes = cn("block", className);
 
-  return <li className={classes}>{children}</li>;
-};
+    return (
+      <li className={classes} ref={ref}>
+        {children}
+      </li>
+    );
+  }
+);
 
 export default memo(ListItem);
