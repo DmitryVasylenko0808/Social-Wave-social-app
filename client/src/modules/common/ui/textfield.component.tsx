d8168bc5e3c866beaac5d459e9603a61c1089278
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-10-18 16:54:33 +0300

    update textfield component (left addon & right addon)

--- client/src/modules/common/ui/textfield.component.tsx
+++ client/src/modules/common/ui/textfield.component.tsx
@@ -4,20 +4,26 @@ import { cn } from "../../../utils/cn";
 type TextFieldProps = ComponentProps<"input"> & {
   label?: string;
   error?: string;
+  leftAddon?: React.ReactNode;
+  rightAddon?: React.ReactNode;
 };
 
 const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ label, error, className, ...inputProps }, ref) => {
+  ({ label, error, leftAddon, rightAddon, className, ...inputProps }, ref) => {
     const classes = cn("flex flex-col", className);
 
     return (
       <div className={classes}>
         <label className="mb-1.5 text-label text-sm">{label}</label>
-        <input
-          className="bg-labelFill border-1 border-textFieldBorder rounded text-xl dark:bg-dark-200 dark:border-dark-300 dark:text-secondary-50"
-          ref={ref}
-          {...inputProps}
-        />
+        <label className="px-3 py-2 flex gap-3 bg-labelFill border-1 border-textFieldBorder rounded text-xl cursor-text dark:bg-dark-200 dark:border-dark-300 dark:text-secondary-50">
+          {leftAddon}
+          <input
+            className="p-0 flex-auto border-0 outline-0 bg-inherit text-xl shadow-none focus:ring-0"
+            ref={ref}
+            {...inputProps}
+          />
+          {rightAddon}
+        </label>
         <p className="text-red-700 text-sm">{error}</p>
       </div>
     );
