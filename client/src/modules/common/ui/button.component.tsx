251c4893dc529b541ff33c162f0b84166e4ddad7
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-10-25 17:03:37 +0300

    update button

--- client/src/modules/common/ui/button.component.tsx
+++ client/src/modules/common/ui/button.component.tsx
@@ -3,7 +3,7 @@ import { cn } from "../../../utils/cn";
 import { Link, LinkProps } from "react-router-dom";
 
 type BaseButtonProps = {
-  variant: "primary" | "secondary" | "tertiary" | "remove";
+  variant: "primary" | "secondary" | "tertiary";
 };
 
 type ButtonAsButtonProps = ComponentProps<"button"> &
@@ -22,8 +22,6 @@ const Button = ({ children, variant, className, ...btnProps }: ButtonProps) => {
   const classes = cn(
     "inline-flex items-center justify-center gap-5",
     {
-      // "px-5 py-4 bg-primary-100 text-white rounded-2xl active:bg-primary-200 disabled:bg-primary-100/40":
-      //   variant === "primary",
       "min-w-btn py-2.5 px-5 bg-primary-200 border border-primary-200 rounded-full text-white font-medium hover:bg-primary-300 active:bg-primary-300":
         variant === "primary",
       "min-w-btn px-5 py-3 bg-secondary-50 text-secondary-300 rounded-full font-medium dark:bg-dark-200 dark:text-secondary-50":
