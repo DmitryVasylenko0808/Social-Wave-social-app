45432d667207bf41ce0a478a358ce5fd38e2a158
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-10-29 15:01:56 +0300

    connect & disconnect user

--- server/src/messages/messages.gateway.ts
+++ server/src/messages/messages.gateway.ts
@@ -8,12 +8,17 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { ChatCreatePayload } from './types/chat.create.payload';
+import { ChatsService } from './services/chats.service';
 
 @WebSocketGateway()
 export class MessagesGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private readonly logger = new Logger('MessagesGateway');
+  private readonly usersMap = new Map<string, string>();
 
   @WebSocketServer() wss: Server;
+
+  constructor(private readonly chatsService: ChatsService) {}
 
   afterInit(server: any) {
     this.logger.log('Initialized');
@@ -21,14 +26,35 @@ export class MessagesGateway implements OnGatewayInit, OnGatewayConnection, OnGa
 
   handleConnection(client: Socket, ...args: any[]) {
     this.logger.log('Connected');
+
+    const { userId } = client.handshake.query;
+
+    this.usersMap.set(userId as string, client.id);
+
+    console.log(this.usersMap);
   }
 
   handleDisconnect(client: Socket) {
     this.logger.log('Disconnected');
+
+    let userId: string;
+
+    for (const [k, v] of this.usersMap.entries()) {
+      if (v === client.id) {
+        userId = k;
+
+        break;
+      }
+    }
+
+    this.usersMap.delete(userId);
+
+    console.log(this.usersMap);
   }
 
-  @SubscribeMessage('messages:test')
-  handleMessage(client: Socket, payload: any) {
-    client.emit('test', 'Hello!');
-  }
+  // @SubscribeMessage('chats:create')
+  // async handleMessage(client: Socket, payload: ChatCreatePayload) {
+  //   await this.chatsService.create(payload);
+  //   client.emit('chats', 'Hello!');
+  // }
 }
