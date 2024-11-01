4caddd51230b77fb486524772b6127386025b5a2
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-11-01 15:42:44 +0300

    update chat list & chat item components

--- client/src/modules/messages/components/chat.item.tsx
+++ client/src/modules/messages/components/chat.item.tsx
@@ -3,7 +3,7 @@ import { Avatar } from "../../common/ui";
 
 const ChatItem = () => {
   return (
-    <div className="flex items-center gap-5">
+    <div className="p-4 flex items-center gap-5 cursor-pointer hover:bg-primary-100/20">
       <Avatar variant="medium" src={`${userAvatarsUrl}/nullavatar.jpg`} />
       <span className="text-secondary-300 font-medium dark:text-white">
         Lorem Ipsum
--- client/src/modules/messages/components/chats.list.tsx
+++ client/src/modules/messages/components/chats.list.tsx
@@ -4,43 +4,41 @@ import ChatItem from "./chat.item";
 const ChastList = () => {
   return (
     <div className="h-[calc(100vh-90px-64px)] overflow-y-scroll">
-      <div className="p-4">
-        <List className="gap-8">
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-          <ListItem>
-            <ChatItem />
-          </ListItem>
-        </List>
-      </div>
+      <List className="gap-0">
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+        <ListItem>
+          <ChatItem />
+        </ListItem>
+      </List>
     </div>
   );
 };
