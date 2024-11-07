9baf311e7fae21197cbd0f9ed5aa4d922fde3900
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-11-07 15:53:53 +0300

    scroll to bottom message list

--- client/src/modules/messages/components/messages.list.tsx
+++ client/src/modules/messages/components/messages.list.tsx
@@ -2,6 +2,7 @@ import { useAuth } from "../../auth/hooks/useAuth";
 import { List, ListItem } from "../../common/ui";
 import MessageItem from "./message.item";
 import { GetMessagesDto, Message } from "../api/dto/get.messages.dto";
+import { useEffect, useRef } from "react";
 
 type MessagesListProps = {
   data: GetMessagesDto;
@@ -15,24 +16,32 @@ const MessagesList = ({
   onClickMessages,
 }: MessagesListProps) => {
   const { user } = useAuth();
+  const endRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [data]);
 
   return (
-    <List className="gap-0">
-      {data?.map((message) => (
-        <ListItem key={message._id}>
-          <MessageItem
-            message={message}
-            selected={message._id === selectedMessage?._id}
-            variant={user.userId === message.user._id ? "user" : "other"}
-            onClick={
-              user.userId === message.user._id
-                ? () => onClickMessages(message)
-                : undefined
-            }
-          />
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <List className="gap-0">
+        {data?.map((message) => (
+          <ListItem key={message._id}>
+            <MessageItem
+              message={message}
+              selected={message._id === selectedMessage?._id}
+              variant={user.userId === message.user._id ? "user" : "other"}
+              onClick={
+                user.userId === message.user._id
+                  ? () => onClickMessages(message)
+                  : undefined
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+      <div ref={endRef} />
+    </>
   );
 };
 
