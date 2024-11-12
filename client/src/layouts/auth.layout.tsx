11d917b310cabe236b2b7db4544e6b20880e3696
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-11-12 13:56:07 +0300

    update auth layout

--- client/src/layouts/auth.layout.tsx
+++ client/src/layouts/auth.layout.tsx
@@ -6,7 +6,7 @@ import { Loader } from "../modules/common/ui";
 const AuthLayout = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center dark:bg-dark-100">
-      <Logo className="mb-20" />
+      <Logo className="mb-9" />
       <Suspense fallback={<Loader position="center" />}>
         <Outlet />
       </Suspense>
