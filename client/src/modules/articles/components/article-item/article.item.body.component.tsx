7e4589d7d456b64dd5dcb59ac4dc345b8d16a0c5
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-11-12 13:55:59 +0300

    update article images

--- client/src/modules/articles/components/article-item/article.item.body.component.tsx
+++ client/src/modules/articles/components/article-item/article.item.body.component.tsx
@@ -8,10 +8,10 @@ const ArticleItemBody = ({ data }: ArticleItemBodyProps) => {
     <div>
       <p className="mb-2 dark:text-secondary-100">{data?.text}</p>
       {!!data.images?.length && (
-        <div className="mb-2 flex flex-wrap gap-4">
+        <div className="mb-2 flex flex-wrap gap-4 max-md:flex-col">
           {data.images.map((image, index) => (
             <img
-              className="size-fit h-[200px] rounded-xl"
+              className="size-fit h-[200px] rounded-xl max-md:w-full max-md:h-[260px]"
               src={`${articlesImgUrl}/${image}`}
               alt={`image ${index}`}
               key={index}
